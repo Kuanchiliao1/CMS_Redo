ba4863c9c36e37d0bc8839b58b923e3559ef76c2
kuanchiliao1 <kuanchiliao@gmail.com>
2022-10-26 18:00:39 -0400

    Add links to display files

--- cms.rb
+++ cms.rb
@@ -7,18 +7,24 @@ require 'tilt/erubis'
 # Absolute path to this directory
 ROOT = File.expand_path("..", __FILE__)
 
+helpers do
+  # Displays list of files from the data directory
+  def display_files
+    list_items = Dir.glob(ROOT + "/data/*").map do |file_path|
+      basename = File.basename(file_path)
+      "<li><a href='/#{basename}'>#{basename}</a></li>"
+    end.join
+
+    "<ul>#{list_items}</ul>"
+  end
+end
+
 get "/" do
   "getting started"
   erb :index
 end
 
-helpers do
-  # Displays list of files from the data directory
-  def display_files
-    list_items = Dir.glob(ROOT + "/data/*").map do |file_path|
-      "<li>#{File.basename(file_path)}</li>"
-    end.join
-
-    "<ul>#{list_items}</ul>"
-  end
+get "/:filename" do |filename|
+  @content = File.read(ROOT + "/data/" + filename)
+  erb :file
 end
